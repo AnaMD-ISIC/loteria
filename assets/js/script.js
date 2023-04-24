ec4f47856609b33f00565962a44c18268dec0c48
Ana MD <ana.mejia19@sistemas.tecsanpedro.edu.mx>
2023-04-23 19:06:59 -0600

    generar cartas random + botones funcionales

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,25 @@
-/*
-OPCION 1: NOP
-var cardArray = new Array[53];
+let inter;
 
-for (var i = 0; i<=cardArray.length; i++) {
-  cardArray[i] = new Image();
-  cardArray[i].src = ('assets/img/baraja/' + i + '.jpg');
+function randomNum (min, max){
+  return Math.floor(Math.random() * max) + min;
 }
 
-var mainCard = document.getElementById("main-card");
-for (var i = 0; i<=cardArray.length; i++) {
-  document.getElementById("main-card").src = cardArray[i].src;
-}*/
-
-/*
-OPCIÓN 2: MENOS
-
-var images = ["./assets/img/card1.jpg",
-"./assets/img/card2.jpg",
-"./assets/img/card3.jpg",
-"./assets/img/card4.jpg",
-"./assets/img/card5.jpg",
-"./assets/img/card6.jpg",
-"./assets/img/card7.jpg"];
-var index = 0;
-
-function makeImage() {
-   var img = document.createElement('img')
-   img.src = images[index];
-   document.getElementById('main-card').appendChild(img);
+function changeSRC(){
+  var random = randomNum(1,54);
+  document.getElementById("main-card").src="assets/img/"+ random +".jpg";
 }
 
-function nextImage(){
-   var img = document.getElementById('main-card').getElementsByTagName('img')[0]
-   index++;
-   index = index % images.length; 
-   img.src = images[index];
-}*/
+function cardStart() {
+   inter = setInterval(changeSRC, 3000);
+}
 
-/*OPCIÓN 3:*/
+function cardStop() {
+  clearInterval(inter);
+    inter = null;
+}
 
-var i = Math.round(Math.random()*7);
-var cardArray = new Array(54);
-
-cardArray[0] = "./img/card1.jpg";
-cardArray[1] = "./img/card2.jpg";
-cardArray[2] = "./img/card3.jpg";
-cardArray[3] = "./img/card4.jpg";
-cardArray[4] = "./img/card5.jpg";
-cardArray[5] = "./img/card6.jpg";
-cardArray[6] = "./img/card7.jpg";
-
-function cargarImagen(){
-  var img = document.getElementById('main-card');
-  document.img.src = cardArray[i];
+function cardReload () {
+  document.getElementById("main-card").src="assets/img/back.jpg";
+  clearInterval(inter);
+    inter = null;
 }
